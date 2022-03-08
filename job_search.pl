039f78e82f6cf331201d4a2aa8ad3b093e7c29c0
a464068 <andreagiuseppe.diiura@enel.com>
2022-03-08 14:27:47 +0100

    Updates

--- job_search.pl
+++ job_search.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings;
-use Pod::Usage;
-use Getopt::Long;
-use File::Slurp;
+use strict ;
+use warnings ;
+use Pod::Usage ;
+use Getopt::Long ;
+use File::Slurp ;
 
 #use Browser::Open qw( open_browser );
 
@@ -24,74 +24,71 @@ use File::Slurp;
 
 =cut
 
-my $verbose = 0;
-my $keys_file = "keys.conf";
-my $locations_file = "locations.conf";
+my $verbose        = 0 ;
+my $keys_file      = "keys.conf" ;
+my $locations_file = "locations.conf" ;
 
-GetOptions (
+GetOptions(
     "f|verbose!"      => \$verbose,
-	"keys=s" 	      => \$keys_file,
-	"keyslocations=s" => \$locations_file,
-)
-or pod2usage( "Try '$0 --help' for more information." );
+    "keys=s"          => \$keys_file,
+    "keyslocations=s" => \$locations_file,
+    )
+    or pod2usage( "Try '$0 --help' for more information." ) ;
 
 # TODO: fix open in WSL!!!
 
 sub open_default_browser {
-	my $url = shift;
-	my $platform = $^O;
+    my $url      = shift ;
+    my $platform = $^O ;
 
-	my $cmd;
+    my $cmd ;
 
-	if ($platform eq 'darwin') {
-		$cmd = "open \"$url\""; # Mac OS X
-	}
-	elsif ($platform eq 'linux') {
-		$cmd = "x-www-browser \"$url\"";  # Linux
-	}
-	elsif ($platform eq 'MSWin32') {
-		$cmd = "start $url";  # Win95..Win7
-	}
+    if ( $platform eq 'darwin' ) {
+        $cmd = "open \"$url\"" ;    # Mac OS X
+    } elsif ( $platform eq 'linux' ) {
+        $cmd = "x-www-browser \"$url\"" ;    # Linux
+    } elsif ( $platform eq 'MSWin32' ) {
+        $cmd = "start $url" ;                # Win95..Win7
+        }
 
-	if (defined $cmd) {
-	    system($cmd);
- 	} else {
-   		die "Can't locate default browser $!";
-  	}
-}
+    if ( defined $cmd ) {
+        system( $cmd) ;
+    } else {
+        die "Can't locate default browser $!" ;
+        }
+    }
 
 # load external files
-my @keywords = read_file($keys_file, chomp => 1);
-my @locations = read_file($locations_file, chomp => 1);
+my @keywords  = read_file( $keys_file,      chomp => 1 ) ;
+my @locations = read_file( $locations_file, chomp => 1 ) ;
 
-my $basic_url = "https://it.indeed.com/offerte-lavoro\?";
-my $query = "q\=";
-my $loc = "\&l\=";
-my $end_url = "\&fromage\=last";
+my $basic_url = "https://it.indeed.com/offerte-lavoro\?" ;
+my $query     = "q\=" ;
+my $loc       = "\&l\=" ;
+my $end_url   = "\&fromage\=last" ;
 
 sub main {
-	foreach my $key (@keywords) {
-		foreach my $location (@locations) {
-			my @kwds = split ' ', $key;
+    foreach my $key ( @keywords ) {
+        foreach my $location ( @locations ) {
+            my @kwds = split ' ', $key ;
 
-			my $my_query = $query;
-			foreach my $k (@kwds)
-			{
-				$my_query = $my_query.$k."+";
-			}
-			chop($my_query);
+            my $my_query = $query ;
+            foreach my $k ( @kwds ) {
+                $my_query = $my_query . $k . "+" ;
+                }
+            chop( $my_query ) ;
 
-			my $url = $basic_url.$my_query.$loc.$location.$end_url;
+            my $url = $basic_url . $my_query . $loc . $location . $end_url ;
 
-			if ($verbose > 0) {
-				print $url."\n";
-			};
-			open_default_browser($url);
-		}
-	}
+            if ( $verbose > 0 ) {
+                print $url. "\n" ;
+                }
+            open_default_browser( $url ) ;
+            }
+        }
 
-}
+    }
 
 ### end of code
-main();
-print "\nEND OF CODE\n";
+main() ;
+print "\nEND OF CODE\n" ;
